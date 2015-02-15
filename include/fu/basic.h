6a04482a1ecc954ec9291fb9a856fa810aaf2786
Scott Prager <splinterofchaos@gmail.com>
2015-02-15 14:56:13 -0500

    invoke: Don't forward member pointers.
    Unnesessary and causes enable_if to fail.

--- include/fu/basic.h
+++ include/fu/basic.h
@@ -35,21 +35,21 @@ constexpr struct invoke_f {
   // Member function overloads:
 
   template<class F, class O, class...X>
-  constexpr auto operator()(F&& f, O&& o, X&&...x) const
+  constexpr auto operator()(F f, O&& o, X&&...x) const
     -> decltype((std::declval<O>().*f)(std::declval<X>()...))
   {
     return (std::forward<O>(o).*f)(std::forward<X>(x)...);
   }
 
   template<class F, class O, class...X>
-  constexpr auto operator() (F&& f, O&& o, X&&...x) const
+  constexpr auto operator() (F f, O&& o, X&&...x) const
     -> decltype((std::declval<O>()->*f)(std::declval<X>()...))
   {
     return (std::forward<O>(o)->*f)(std::forward<X>(x)...);
   }
 
   template<class F, class O>
-  constexpr auto operator() (F&& f, O&& o) const
+  constexpr auto operator() (F f, O&& o) const
     -> enable_if_t< std::is_member_object_pointer<F>::value
                   , decltype(std::declval<O>().*f)>
   {
@@ -57,11 +57,11 @@ constexpr struct invoke_f {
   }
 
   template<class F, class O>
-  constexpr auto operator() (F&& f, O&& o) const
+  constexpr auto operator() (F f, O&& o) const
     -> enable_if_t< std::is_member_object_pointer<F>::value
                   , decltype(std::declval<O>()->*std::declval<F>())>
   {
-    return std::forward<O>(o)->*std::forward<F>(f);
+    return std::forward<O>(o)->*f;
   }
 } invoke{};
 
