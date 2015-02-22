7570a71e983cc5110d2dfb0c17aa5bd675c2ab87
Scott Prager <splinterofchaos@gmail.com>
2015-02-22 17:27:14 -0500

    Basic travis.yml.
    Disable clang--just can't get it working!

---
+++ run-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$CXX" = "clang++" ]; then export EXTRA="-stdlib=libc++ -I/usr/include/c++/v1"; fi
+
+for file in test/*
+do
+  echo "compiling ${file}..."
+  $CXX $file -std=c++14 -Iinclude -Wall -Wextra $EXTRA || exit 1
+  ./a.out || exit 1
+done
