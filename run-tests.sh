1748ada5d4b653c735b40ef7bdb831155adcee6f
Scott Prager <splinterofchaos@gmail.com>
2015-02-23 09:44:16 -0500

    travis: compile with -Werror

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,6 @@ if [ "$CXX" = "clang++" ]; then export EXTRA="-stdlib=libc++ -I/usr/include/c++/
 for file in test/*
 do
   echo "compiling ${file}..."
-  $CXX $file -std=c++14 -Iinclude -Wall -Wextra $EXTRA || exit 1
+  $CXX $file -std=c++14 -Iinclude -Wall -Wextra -Werror $EXTRA || exit 1
   ./a.out || exit 1
 done
