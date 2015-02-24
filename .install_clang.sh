7a1815b884b83155a751677e20598f5f49e6dc05
Scott Prager <splinterofchaos@gmail.com>
2015-02-24 15:08:39 -0500

    travis/clang: install libcxx from source

--- .install_clang.sh
+++ .install_clang.sh
@@ -5,4 +5,15 @@ sudo apt-add-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise
 sudo apt-get -qq update
 sudo apt-get -qq --force-yes install clang-3.5 libc++-dev
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.5 1
-sudo rm /usr/local/clang-3.4/bin/clang++
+export CXXFLAGS="-std=c++14 -stdlib=libc++"
+
+svn co --quiet http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+
+cd libcxx/lib && bash buildit
+sudo cp ./libc++.so.1.0 /usr/lib/
+sudo mkdir /usr/include/c++/v1
+cd .. && sudo cp -r include/* /usr/include/c++/v1/
+cd /usr/lib && sudo ln -sf libc++.so.1.0 libc++.so
+sudo ln -sf libc++.so.1.0 libc++.so.1 && cd $cwd
+
+CXX="clang++-3.5"
--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-if [ "$CXX" = "clang++" ]; then export EXTRA="-stdlib=libc++ -I/usr/include/c++/v1"; fi
-
 for file in test/*
 do
   echo "compiling ${file}..."
-  $CXX $file -std=c++14 -Iinclude -Wall -Wextra -Werror $EXTRA || exit 1
+  $CXX $file -std=c++14 -Iinclude -Wall -Wextra -Werror $CXXFLAGS || exit 1
   ./a.out || exit 1
 done
